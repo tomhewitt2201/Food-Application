90418a778e3f9a598ad3a12666d29831d1cc9591
Thomas Hewitt <37849834+tomhewitt2201@users.noreply.github.com>
2019-09-17 17:09:56 +0100

    create meals-veg.js
    Just contains an array of the vegetable dinners. 
    
    Currently very simple, but eventually it's going to be built in such a way so that someone can add information ie. ingredients etc. and that will populate and save the data in a class.

---
+++ meals-veg.js
@@ -0,0 +1,13 @@
+// Eventually you're going to turn this into a class. 
+
+const veg = [
+    'Pomodori Col Riso',
+    'Vegetable Kurma',
+    'Vegetable Chilli',
+    'Pasta Arrabbiata',
+    'Pasta con pomodori',
+    'French Onion Soup',
+    'Macaroni Cheese',
+    'Stuffed Peppers',
+    'Roast Dinner (veg)'
+]
