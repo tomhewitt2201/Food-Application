1cdccb39fbbfe328f18433a04c06074f151301be
Thomas Hewitt <37849834+tomhewitt2201@users.noreply.github.com>
2019-09-17 17:11:43 +0100

    create meals-meat.js
    Same story as the vegetable dinners array. 
    
    To avoid repeating code what I might do is create a separate file that handles creating the classes etc. and this is just where it keeps everything.

---
+++ meals-meat.js
@@ -0,0 +1,13 @@
+// eventually this will be a class
+
+const meat = [
+    'Roast Dinner (Meat)',
+    'Peach Chicken',
+    'Chilli con Carne',
+    'Spaghetti con Ragu',
+    'Spaghetti Carbonara',
+    'Chicken Curry',
+    'Pollo alla Cacciatura',
+    'Chorizo and Asparagus Frittata',
+    'Chicken Red Thai Curry'
+]
